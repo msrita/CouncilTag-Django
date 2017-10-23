b87df9df95a8941495f67e8c183bd82f2a412c08
Jeff Yamamoto <jeffmagician@gmail.com>
2017-10-22 20:26:53 -0700

    Add sections for contact and change color

--- client/src/component/Landing.jsx
+++ client/src/component/Landing.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import styles from './landing.scss'
+import styles from './Landing.scss'
 
 const Landing = (props) =>{
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+      <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
         <div className="container">
           <a className="navbar-brand js-scroll-trigger" href="#page-top">Engage</a>
           <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -33,11 +33,11 @@ const Landing = (props) =>{
         <div className="container">
           <img className="img-fluid" src="http://d33wubrfki0l68.cloudfront.net/ac40c001c60109040dc17c9b42d270616cd9e711/2ac65/images/logo-hfla.svg"/>
           <span className="name">Engage</span>
-          <hr class="star-light"/>
+          <hr className="star-light"/>
           <span className="description">Engage helps you voice your opinions at local goverment meetings</span>
         </div>
       </header>
-      <section className="success" id="about">
+      <section className="about" id="about">
         <div className="container">
           <h2 className="text-center">About</h2>
           <hr className="star-light" />
@@ -50,6 +50,34 @@ const Landing = (props) =>{
                 <i className="fa fa-download"></i>
                 How Do I Start?
               </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="tutorial" id="tutorial">
+        <div className="container">
+          <h2 className="text-center">How does City Council work?</h2>
+          <hr className="star-light" />
+          <div className="row">
+            <div className="col-lg-4 ml-auto">
+              <p>It's a bit daunting but we are here to help</p>
+            </div>
+            <div className="col-lg-8 mx-auto text-center">
+              <a href="#" className="btn btn-lg btn-outline">
+                <i className="fa fa-download"></i>
+                Tutorial
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="contact" id="contact">
+        <div className="container">
+          <h2 className="text-center">Contact</h2>
+          <hr className="star-light" />
+          <div className="row">
+            <div className="col-lg-4 ml-auto">
+              <p>Engage helps you voice your opinions at local goverment meetings</p>
             </div>
           </div>
         </div>
