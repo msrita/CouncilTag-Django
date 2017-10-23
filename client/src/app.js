3c99b4793950417fba88cee818e311095399a23a
Jeff Yamamoto <jeffmagician@gmail.com>
2017-10-22 20:00:30 -0700

    Add example logo page and remove unused description

--- client/src/app.js
+++ client/src/app.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-// import ExampleContainer from './container/ExampleContainer.jsx';
 import Landing from './component/Landing.jsx'
 import 'babel-polyfill';
 
-// ReactDOM.render(<ExampleContainer />, document.getElementById('app'));
 ReactDOM.render(<Landing />, document.getElementById('app'));
--- client/src/component/Landing.jsx
+++ client/src/component/Landing.jsx
@@ -29,16 +29,21 @@ const Landing = (props) =>{
           </div>
         </div>
       </nav>
+      <header className="masthead">
+        <div className="container">
+          <img className="img-fluid" src="http://d33wubrfki0l68.cloudfront.net/ac40c001c60109040dc17c9b42d270616cd9e711/2ac65/images/logo-hfla.svg"/>
+          <span className="name">Engage</span>
+          <hr class="star-light"/>
+          <span className="description">Engage helps you voice your opinions at local goverment meetings</span>
+        </div>
+      </header>
       <section className="success" id="about">
         <div className="container">
-          <h2 className="text-center">Engage</h2>
+          <h2 className="text-center">About</h2>
           <hr className="star-light" />
           <div className="row">
             <div className="col-lg-4 ml-auto">
               <p>Engage helps you voice your opinions at local goverment meetings</p>
-            </div>
-            <div className="col-lg-4 mr-auto">
-              <p>Use our app</p>
             </div>
             <div className="col-lg-8 mx-auto text-center">
               <a href="#" className="btn btn-lg btn-outline">
@@ -49,19 +54,7 @@ const Landing = (props) =>{
           </div>
         </div>
       </section>
-      <section className="success" id="about">
-        <div className="container">
-          <h2 className="text-center">About</h2>
-          <hr className="star-light" />
-          <div className="row">
-            <div className="col-lg-4 ml-auto">
-              <p>Engage helps you voice your opinions at local goverment meetings</p>
-            </div>
-          </div>
-        </div>
-      </section>
     </div>
   );
 }
-
 export default Landing;
